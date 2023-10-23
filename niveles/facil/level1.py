5392cc17a2425bc140e7fd6fc65e1469327b320d
jfilippo06 <jfilippo04@gmail.com>
2023-10-23 21:49:48 +0200

    modular

---
+++ niveles/facil/level1.py
@@ -0,0 +1,39 @@
+import pygame, sys
+from clases.button import Button
+from pantallas import menu
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((700, 600))
+pygame.display.set_caption("Nivel 1")
+
+BG = pygame.image.load("assets/Background.png")
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
+
+def level1():
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("black")
+
+        PLAY_TEXT = get_font(40).render("NIVEL 1", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(370, 200))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = Button(image=None, pos=(370, 300), 
+                            text_input="VOLVER", font=get_font(30), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    menu.main_menu()
+
+        pygame.display.update()
---
+++ pantallas/menu.py
@@ -0,0 +1,47 @@
+import pygame, sys
+from clases.button import Button
+from niveles.facil import level1
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((700, 600))
+pygame.display.set_caption("Menu")
+
+BG = pygame.image.load("assets/Background.png")
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
+
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(40).render("NOMBRE DEL JUEGO", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(360, 110))
+        
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(360, 250), 
+                            text_input="INICIAR", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(360, 370), 
+                            text_input="SALIR", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    level1.level1()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
